9844a5b19f159ba37a845131b1cef5e13fd9af6c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-11 21:14:29 +0000

    Deploy mrkosima/playground-4square to github.com/mrkosima/playground-4square.git:gh-pages

---
+++ precache-manifest.d0330521f1543b76adbe53905829a6eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bce1a11d9ddf24e765a",
+    "url": "/playground-4square/static/css/main.a53fd283.chunk.css"
+  },
+  {
+    "revision": "3bce1a11d9ddf24e765a",
+    "url": "/playground-4square/static/js/main.3bce1a11.chunk.js"
+  },
+  {
+    "revision": "9696ca6e76604b8a47a3",
+    "url": "/playground-4square/static/css/1.d0f3828d.chunk.css"
+  },
+  {
+    "revision": "9696ca6e76604b8a47a3",
+    "url": "/playground-4square/static/js/1.9696ca6e.chunk.js"
+  },
+  {
+    "revision": "19ab51cfe03a8fcddbcd",
+    "url": "/playground-4square/static/js/runtime~main.19ab51cf.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/playground-4square/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/playground-4square/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/playground-4square/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/playground-4square/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "b0aa05eeb6f121ea07a4c31ff6f06af9",
+    "url": "/playground-4square/static/media/location.b0aa05ee.svg"
+  },
+  {
+    "revision": "76d43bfbd4b2b3ab22cca7f4a24c8ae0",
+    "url": "/playground-4square/index.html"
+  }
+];
